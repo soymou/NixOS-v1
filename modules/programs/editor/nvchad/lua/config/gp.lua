73a9cb4ba1e83b571409f97822e740f8e220d517
emilio-junoy <emilio.junoy@gmail.com>
2025-07-22 11:34:18 -0600

    hola

--- modules/programs/editor/nvchad/lua/config/gp.lua
+++ modules/programs/editor/nvchad/lua/config/gp.lua
@@ -1,23 +1,18 @@
-print("== Starting gp.lua config ==")
-
 require("gp").setup {
   openai_api_key = false,
   openai_api_endpoint = "http://localhost:11434/v1",
+
   agents = {
     {
       name = "llama3",
       model = "llama3:latest",
       provider = "ollama",
-      provider_opts = {
-        api_base = "http://localhost:11434/v1",
-      },
       system_prompt = "You're a helpful coding assistant.",
       temperature = 0.5,
     },
   },
+
   default_agent = "llama3",
   log_level = "debug",
 }
-
-print("== Finished gp.lua config ==")
 
