85949b9c1b68efd291a8b207dbc76bcfe173cf0f
emilio-junoy <emilio.junoy@gmail.com>
2025-07-22 10:29:54 -0600

    hola

--- modules/programs/editor/nvchad/lua/config/chadrc.lua
+++ modules/programs/editor/nvchad/lua/config/chadrc.lua
@@ -3,18 +3,8 @@
 local M = {}
 
 M.ui = {
-  theme = "nord",           -- Use NvChad's built-in Nord theme
-  -- Optional: add toggle pair
-  theme_toggle = { "nord", "onedark" }, 
-
-  changed_themes = {
-    nord = function()
-      vim.cmd([[colorscheme nord]])
-    end,
-    onedark = function()
-      vim.cmd([[colorscheme onedark]])
-    end,
-  },
+  theme = "nord",                 -- Use built-in Nord theme
+  theme_toggle = { "nord", "onedark" },  -- Optional toggle
 }
 
 return M
