7d64a98337d81a0df8f882b32cc06bd4e53f1e28
emilio-junoy <emilio.junoy@gmail.com>
2025-07-22 10:41:37 -0600

    hola

--- modules/programs/editor/nvchad/lua/config/chadrc.lua
+++ modules/programs/editor/nvchad/lua/config/chadrc.lua
@@ -3,8 +3,8 @@
 local M = {}
 
 M.ui = {
-  theme = "nord",                 -- Use built-in Nord theme
-  theme_toggle = { "nord", "onedark" },  -- Optional toggle
+  theme = "onenord",                 -- Use built-in Nord theme
+  theme_toggle = { "onenord", "onedark" },  -- Optional toggle
 }
 
 return M
