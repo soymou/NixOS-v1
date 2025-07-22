0792c7f05687bebec9452e3a9dda86636db5db35
emilio-junoy <emilio.junoy@gmail.com>
2025-07-22 12:52:27 -0600

    hola

--- modules/programs/editor/nvchad/lua/config/luasnip.lua
+++ modules/programs/editor/nvchad/lua/config/luasnip.lua
@@ -1,42 +1,36 @@
-local ls = require("luasnip")
 local cmp = require("cmp")
+local luasnip = require("luasnip")
 
-ls.config.set_config({
-  history = true,
-  updateevents = "TextChanged,TextChangedI",
-  enable_autosnippets = true,
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+  },
 })
-
--- Tab completion function for insert/select modes
-function _G.tab_complete(fallback)
-  local col = vim.fn.col('.') - 1
-  local line = vim.fn.getline('.')
-
-  if cmp.visible() then
-    cmp.select_next_item()
-  elseif ls.expand_or_jumpable() then
-    ls.expand_or_jump()
-  elseif col == 0 or line:sub(col, col):match("%s") then
-    fallback()  -- Insert a real tab character
-  else
-    cmp.complete()
-  end
-end
-
-function _G.shift_tab_complete(fallback)
-  if cmp.visible() then
-    cmp.select_prev_item()
-  elseif ls.jumpable(-1) then
-    ls.jump(-1)
-  else
-    fallback()
-  end
-end
-
--- Map Tab and Shift-Tab keys in insert and select modes to our functions
-vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", { expr = true, silent = true })
-vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", { expr = true, silent = true })
-vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.shift_tab_complete()", { expr = true, silent = true })
-vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.shift_tab_complete()", { expr = true, silent = true })
-
-require("luasnip.loaders.from_lua").lazy_load({ paths = { "~/.config/nvim/snippets" } })
