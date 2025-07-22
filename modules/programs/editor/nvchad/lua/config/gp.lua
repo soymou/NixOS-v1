b6dda89bd29eb48603b2439b0c65cb7e551451b6
emilio-junoy <emilio.junoy@gmail.com>
2025-07-22 11:32:55 -0600

    hola

--- modules/programs/editor/nvchad/lua/config/gp.lua
+++ modules/programs/editor/nvchad/lua/config/gp.lua
@@ -1,6 +1,8 @@
+print("== Starting gp.lua config ==")
+
 require("gp").setup {
   openai_api_key = false,
-
+  openai_api_endpoint = "http://localhost:11434/v1",
   agents = {
     {
       name = "llama3",
@@ -13,7 +15,9 @@ require("gp").setup {
       temperature = 0.5,
     },
   },
-
   default_agent = "llama3",
   log_level = "debug",
 }
+
+print("== Finished gp.lua config ==")
+
