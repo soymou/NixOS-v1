3d1015b280ada265399215e17f90cb0094d72db5
emilio-junoy <emilio.junoy@gmail.com>
2025-07-23 12:29:08 -0600

    hola

--- modules/programs/editor/nvchad/snippets/tex.lua
+++ modules/programs/editor/nvchad/snippets/tex.lua
@@ -95,25 +95,25 @@ return {
 ]], { i(1) })),
 
   -- Inline math
-  s({ trig = "mk$", regTrig = true, snippetType = "autosnippet" }, fmt("\\( {} \\)", { i(1) })),
+  s({ trig = "mk", snippetType = "autosnippet" }, fmt("\\( {} \\)", { i(1) })),
 
   -- \mathbb{}
-  s({ trig = "mbb$", regTrig = true, snippetType = "autosnippet" }, fmt("\\mathbb{{{}}}", { i(1) })),
+  s({ trig = "mbb", snippetType = "autosnippet" }, fmt("\\mathbb{{{}}}", { i(1) })),
 
   -- \mathcal{}
-  s({ trig = "mcal$", regTrig = true, snippetType = "autosnippet" }, fmt("\\mathcal{{{}}}", { i(1) })),
+  s({ trig = "mcal", snippetType = "autosnippet" }, fmt("\\mathcal{{{}}}", { i(1) })),
 
   -- Bold (context-aware)
-  s({ trig = "bf$", regTrig = true, snippetType = "autosnippet", condition = in_textzone },
+  s({ trig = "bf", snippetType = "autosnippet", condition = in_textzone },
     fmt("\\textbf{{{}}}", { i(1) })),
 
-  s({ trig = "bf$", regTrig = true, snippetType = "autosnippet", condition = in_mathzone },
+  s({ trig = "bf", snippetType = "autosnippet", condition = in_mathzone },
     fmt("\\mathbf{{{}}}", { i(1) })),
 
   -- Italic (context-aware)
-  s({ trig = "if$", regTrig = true, snippetType = "autosnippet", condition = in_textzone },
+  s({ trig = "if", snippetType = "autosnippet", condition = in_textzone },
     fmt("\\textit{{{}}}", { i(1) })),
 
-  s({ trig = "if$", regTrig = true, snippetType = "autosnippet", condition = in_mathzone },
+  s({ trig = "if", snippetType = "autosnippet", condition = in_mathzone },
     fmt("\\mathit{{{}}}", { i(1) })),
 }
